43ee04ee50a305c8c7681132a057f4b82f7d070a
Timur <timtch@gmail.com>
2014-10-14 21:15:11 +0200

    update 0.9

--- package.js
+++ package.js
@@ -1,20 +1,39 @@
 Package.describe({
-  summary: "A package to display flash messages to the user."
+  name: 'timtch:flash-messages-plus',
+  summary: "A package to display flash messages to the user.",
+  version: '0.0.1',
+  git: 'https://github.com/timtch/flash-messages'
+
 });
 
-Package.on_use(function(api, where) {
-  api.use(['minimongo', 'mongo-livedata', 'templating', 'coffeescript'], 'client');
+Package.onUse(function(api, where) {
+  api.use([
+    'minimongo', 
+    'mongo-livedata', 
+    'templating', 
+    'coffeescript'
+    ], 'client');
 
-    api.add_files(['messages.coffee', 'messages_list.html', 'messages_list.coffee'], 'client');
+    api.addFiles([
+      'messages.coffee', 
+      'messages_list.html', 
+      'messages_list.coffee'
+      ], 'client');
 
   if (typeof api.export !== 'undefined') {
-    api.export(['FlashMessages', 'flashMessages'], 'client');
+    api.export([
+      'FlashMessages', 
+      'flashMessages'
+      ], 'client');
   }
 });
 
-Package.on_test(function(api) {
+Package.onTest(function(api) {
   api.use('flash-messages-plus', 'client');
-  api.use(['tinytest', 'test-helpers'], 'client');
+  api.use([
+    'tinytest', 
+    'test-helpers'
+  ], 'client');
 
-  api.add_files('messages_tests.js', 'client');
+  api.addFiles('messages_tests.js', 'client');
 });
